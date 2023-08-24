79ad3dc71bef1e464a7c2aebe50c392fa6247dc6
Sh3rlock <fhawaa@gmail.com>
2023-08-24 13:24:32 -0700

    Update RogueCompany.cpp

--- RogueCompany.cpp
+++ RogueCompany.cpp
@@ -9,3 +9,8 @@ Gname     : 0x70F57C0    // 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8B D0 C6 05 ? ? ? ? ?
 Gworld    : 0x7264918  //   48 89 05 ? ? ? ? 48 8B B7 88 00 00 00
 Gobjects  : 0x7121A38   //  48 8B 05 ? ? ? ? 48 8B 0C C8 48 8D 04 D1 EB ? 45 33 FF
 
+GetBoneMatrix = 0x43ba3a5;
+World2Screen = 0x4098420;
+GetObjectName = 0x41241f0;
+
+
