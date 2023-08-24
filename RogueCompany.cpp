dfe776fe51a98d4f038ec56f4a7db53ddbf4c218
Sh3rlock <fhawaa@gmail.com>
2023-08-24 13:17:39 -0700

    Update RogueCompany.cpp

--- RogueCompany.cpp
+++ RogueCompany.cpp
@@ -1,8 +1,11 @@
-RogueCompany
-Old updated :(
-Gname  -> 0x704C900
-Gworld -> 0x71BBA58
+Game      : Rogue Company    [Steam]
 
-New updated :)
-Gname  -> 0x7023100
-Gworld -> 0x7192258
+Base      : 0x7FF7879A0000
+Gnamebase : 0x7FF78EA957C0
+Gworld    : 0x7FF78EC04918
+Gobjects  : 0x7FF78EAC1A38
+
+Gname     : 0x70F57C0    // 48 8D 0D ? ? ? ? E8 ? ? ? ? 48 8B D0 C6 05 ? ? ? ? ? 48 8B 44 24 30 48 C1 E8 ?
+Gworld    : 0x7264918  //   48 89 05 ? ? ? ? 48 8B B7 88 00 00 00
+Gobjects  : 0x7121A38   //  48 8B 05 ? ? ? ? 48 8B 0C C8 48 8D 04 D1 EB ? 45 33 FF
+
